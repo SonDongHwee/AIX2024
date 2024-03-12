bd3154aac00c4b6235e06acc1d7e5e55cc57482a
최호승 <choehoseung@choehoseungs-Laptop.local>
2024-03-13 00:55:59 +0900

    reference code 2023

---
+++ AIX-main-2/bin/tiny-yolo-aix2023.sh
@@ -0,0 +1,2 @@
+#./darknet detector map yolohw.names yolov3-tiny-aix2022.cfg yolov3-tiny-aix2022.weights -thresh 0.24
+./darknet detector map yolohw.names yolov4-tiny-aix2023.cfg yolov4-tiny-aix2023.weights -thresh 0.24
