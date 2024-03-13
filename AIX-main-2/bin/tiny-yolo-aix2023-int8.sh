1d199625c59815847045303fbd88866a44541299
hoseung1102 <63445453+hoseung1102@users.noreply.github.com>
2024-03-14 00:33:47 +0900

    Delete AIX-main-2 directory

--- AIX-main-2/bin/tiny-yolo-aix2023-int8.sh
+++
@@ -1,3 +0,0 @@
-#./darknet detector map yolohw.names yolov3-tiny-aix2022.cfg yolov3-tiny-aix2022.weights -thresh 0.24 -quantized -save_params
-./darknet detector map yolohw.names yolov4-tiny-aix2023.cfg yolov4-tiny-aix2023.weights -thresh 0.24 -quantized
-#./darknet detector map yolohw.names yolov3-tiny-aix2022.cfg yolov3-tiny-aix2022.weights -thresh 0.24 -quantized
--- AIX-main-2/bin/yolo_cpu_int8.cmd
+++
@@ -1,8 +0,0 @@
-
-rem yolo_cpu.exe detector map yolohw.names yolov3-tiny-aix2022.cfg yolov3-tiny-aix2022.weights -thresh 0.24 -quantized
-
-
-yolo_cpu.exe detector map yolohw.names yolov3-tiny-aix2022.cfg yolov3-tiny-aix2022.weights -thresh 0.24 -quantized
-
-
-pause
